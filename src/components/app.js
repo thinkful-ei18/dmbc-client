33a1018f9c6deddfe4c6f1fbf81bfe2b6d5cc9e7
Chris Yang <cyang287@yahoo.com>
2018-04-30 15:57:48 -0400

    added redux

--- src/components/app.js
+++ src/components/app.js
@@ -1,16 +1,52 @@
-import React, { Component } from 'react';
+import React from 'react';
+import {connect} from 'react-redux';
+import {withRouter} from 'react-router-dom';
+import {refreshAuthToken} from '../actions/auth';
 
-import '../styles/app.css';
+import '../styles/app.css'
 
-class App extends Component {
+class App extends React.Component {
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.loggedIn && !this.props.loggedIn) {
+      // When we are logged in, refresh the auth token periodically
+      this.startPeriodicRefresh();
+    } else if (!nextProps.loggedIn && this.props.loggedIn) {
+      // Stop refreshing when we log out
+      this.stopPeriodicRefresh();
+    }
+  }
+
+  componentWillUnmount() {
+    this.stopPeriodicRefresh();
+  }
+
+  startPeriodicRefresh() {
+    this.refreshInterval = setInterval(
+      () => this.props.dispatch(refreshAuthToken()),
+      10 * 60 * 1000 // Ten minutes
+    );
+  }
+
+  stopPeriodicRefresh() {
+    if (!this.refreshInterval) {
+      return;
+    }
+    clearInterval(this.refreshInterval);
+  }
+
   render() {
-    return (
+    return(
       <div className="app">
         <p>Hello!</p> 
         <p className="hello">- From Chris' Computer</p>
       </div>
-    );
+    ) 
   }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  hasAuthToken: state.auth.authToken !== null,
+  loggedIn: state.auth.currentUser !== null
+});
+
+export default withRouter(connect(mapStateToProps)(App));
