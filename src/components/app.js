a28bdb2e338312f6e56f0e5528652231d1242227
Chris Yang <cyang287@yahoo.com>
2018-04-30 22:14:30 -0400

    fixed login and register

--- src/components/app.js
+++ src/components/app.js
@@ -6,6 +6,7 @@ import {refreshAuthToken} from '../actions/auth';
 import '../styles/app.css'
 import Login from './login';
 import Register from './register';
+import Dashboard from './dashboard';
 
 class App extends React.Component {
   componentWillReceiveProps(nextProps) {
@@ -43,6 +44,7 @@ class App extends React.Component {
         <Link to="/register">Register</Link>
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
+        <Route path="/dashboard" component={Dashboard} />
       </div>
     ) 
   }
