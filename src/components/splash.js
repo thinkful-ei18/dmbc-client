d7be537b0c3094fdf748d7584772d9aab602b599
Dylan <nielsen.dylan@gmail.com>
2018-05-10 14:23:46 -0700

    Some stuff for the home page

--- src/components/splash.js
+++ src/components/splash.js
@@ -1,8 +1,13 @@
 import React from 'react';
+import Logo from './logo'
+import SplashImage from '../assets/Ludwig-Favre-California-22.jpg'
+import '../styles/splash.css';
 
 export default function Splash(props) {
   return (
-    <div>
+    <div className="splash">
+      <img src={SplashImage} alt=""/>
+      <Logo/>
       <h1>We make the choices that you can't</h1>
       <p>Our application pairs you with an ambassador for your trip so that you can
         receive trusted local suggestions for your trip only for the times you need.</p>
