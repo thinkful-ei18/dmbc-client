50042b87d84084476371ea09b33fc07e6ea3afd2
Dylan <nielsen.dylan@gmail.com>
2018-05-16 08:38:46 -0700

    Fix fixtures

--- src/components/newTrip-form.js
+++ src/components/newTrip-form.js
@@ -45,9 +45,39 @@ class NewTripForm extends Component {
       error = '';
     }
     const fixtures = [
-      {label: 'Chicago', location: {lat: 41.8781, lng: -87.6298}},
-      {label: 'Mexico City', location: {lat: 19.4326, lng: -99.1332}},
-      {label: 'Tokyo', location: {lat: 35.673343, lng: 139.710388}},
+      {
+        gmaps: {
+        address_components: [
+          {
+            long_name: 'Chicago',
+            types: ['locality']
+          }
+        ]
+      },
+      label: 'Chicago',
+      location: {lat: 41.8781, lng: -87.6298}},
+      {
+        gmaps: {
+        address_components: [
+          {
+            long_name: 'Mexico City',
+            types: ['locality']
+          }
+        ]
+      }, 
+      label: 'Mexico City',
+      location: {lat: 19.4326, lng: -99.1332}},
+      {
+        gmaps: {
+        address_components: [
+          {
+            long_name: 'Tokyo',
+            types: ['locality']
+          }
+        ]
+      }, 
+      label: 'Tokyo',
+      location: {lat: 35.673343, lng: 139.710388}},
     ];
     return (
       <div className="trip-form-background">
